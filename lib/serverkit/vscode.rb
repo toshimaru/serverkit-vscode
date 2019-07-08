e9b4847dffefcfa30910c47083694d0ad13ea31b
toshimaru <me@toshimaru.net>
2019-07-09 05:41:32 +0900

    Initialize gem

---
+++ lib/serverkit/vscode.rb
@@ -0,0 +1,8 @@
+require "serverkit/vscode/version"
+
+module Serverkit
+  module Vscode
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/serverkit/vscode/version.rb
@@ -0,0 +1,5 @@
+module Serverkit
+  module Vscode
+    VERSION = "0.1.0"
+  end
+end
