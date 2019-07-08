5934af9b8ef7f2e090bd3a7891ce8eb46438e33f
toshimaru <me@toshimaru.net>
2019-07-09 06:18:16 +0900

    Atom -> VSCode

--- lib/serverkit/resources/vscode_package.rb
+++ lib/serverkit/resources/vscode_package.rb
@@ -1,19 +1,21 @@
-require "serverkit/resources/base"
+# frozen_string_literal: true
+
+require 'serverkit/resources/base'
 
 module Serverkit
   module Resources
-    class AtomPackage < Base
+    class VscodePackage < Base
       attribute :name, required: true, type: String
       attribute :version, type: String
 
       # @note Override
       def apply
-        run_command("apm install #{name_with_version}")
+        run_command("code -install-extension #{name_with_version}")
       end
 
       # @note Override
       def check
-        check_command("apm list --bare --installed #{name} | grep -E '#{name_pattern_with_version}'")
+        check_command("code --list-extensions --show-versions | grep -E '#{name_pattern_with_version}'")
       end
 
       private
